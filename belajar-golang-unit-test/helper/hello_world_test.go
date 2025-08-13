43bf265c0160e99c04da9264be4434ec53d949c4
I Gusti Nyoman Geri A W <geri@smsperkasa.com>
2025-08-13 10:19:02 +0700

    golang unit test 2 (kantor)

--- belajar-golang-unit-test/helper/hello_world_test.go
+++ belajar-golang-unit-test/helper/hello_world_test.go
@@ -1,19 +1,22 @@
 package helper
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldAndi(t *testing.T) {
 	result := HelloWorld("Andi")
 	if result != "Hello Andi" {
-		panic("Result is not Hello Andi")
+		t.Error("Result is not Hello Andi")
 	}
+	fmt.Println("TestHelloWorldAndi Done")
 }
 
 func TestHelloWorldBudi(t *testing.T) {
 	result := HelloWorld("Budi")
 	if result != "Hello Budi" {
-		panic("Result is not Hello Budi")
+		t.Fatal("Result is not Hello Budi")
 	}
+	fmt.Println("TestHelloWorldBudi Done")
 }
